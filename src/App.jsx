530837411796d3f22612f668e178dacb37664dbc
Jonathan Roy <jonaisroy@gmail.com>
2025-08-22 10:24:29 +0300

    skills and experience import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Education from "./Education";
+import Experience from "./Experience";
 import GeneralInfo from "./GeneralInfo";
+import Skills from "./Skills";
 
 function App() {
   return (
     <>
       <GeneralInfo />
       <Education />
+      <Experience />
+      <Skills />
     </>
   );
 }
